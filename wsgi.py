209475a73ab25c88d3ba4c0d094266ac74a4d0ef
winkey <rush@winkey.org>
2014-11-08 21:06:46 -0800

    fix wsgi for django 1.3

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,13 @@ os.environ.setdefault( "DJANGO_SETTINGS_MODULE", "NewWorld.settings" )
 
 # This application object is used by the development server
 # as well as any WSGI server configured to use this file.
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+
+# django 1.3 broken with this
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
+
+
