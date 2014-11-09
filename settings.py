8f4811b3906b5944a290402dc855ca54048a1048
winkey <rush@winkey.org>
2014-11-09 11:07:50 -0800

    another wsgi fix

--- settings.py
+++ settings.py
@@ -106,7 +106,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'NewWorld.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     PROJECT_ROOT + "/templates",
