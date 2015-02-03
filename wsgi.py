aa704efeae424f32875d774d444ffb459952991e
winkey <rush@winkey.org>
2015-02-02 18:19:38 -0800

    fixes for the dependancys upgrade

--- wsgi.py
+++ wsgi.py
@@ -11,7 +11,8 @@ os.environ.setdefault( "DJANGO_SETTINGS_MODULE", "settings" )
 #from django.core.wsgi import get_wsgi_application
 #application = get_wsgi_application()
 
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
 
-
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
